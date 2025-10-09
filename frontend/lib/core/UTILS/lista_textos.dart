39c62e9bd57aeb6f89f280bcd72ca4b6f3208009
MorcegodeObra <geefoliveira@gmail.com>
2025-10-09 16:32:38 -0300

    Transformado em somente backend

--- frontend/lib/core/UTILS/lista_textos.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-List<TextSpan> gerarTextSpan(Map<String, dynamic> dados) {
-  return dados.entries.map((entry) {
-    return TextSpan(
-      text: '${entry.key}: ${entry.value ?? ''}\n',
-    );
-  }).toList();
-}
